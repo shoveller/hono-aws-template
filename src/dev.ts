f18cf4be5b1118232acac21eef2ee1279c0f69c4
cinos81 <cinos81@gmail.com>
2025-05-10 13:49:40 +0900

    fix: update import paths and add logger middleware to the app

--- src/dev.ts
+++ src/dev.ts
@@ -1,5 +1,5 @@
 import { serve } from '@hono/node-server'
-import { app } from './index'
+import { app } from '@/index'
 
 serve({ fetch: app.fetch, port: 8080 }, (info) => {
   console.log(`🚀 Server is running at http://localhost:${info.port}`)
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/aws-lambda'
+import { logger } from 'hono/logger'
 
 export const app = new Hono()
+
+app.use('*', logger())
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
